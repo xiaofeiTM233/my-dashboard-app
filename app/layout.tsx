1518708d831136259befe9a24a96eed16a976725
飞小RAN <55832499+xiaofeiTM233@users.noreply.github.com>
2026-08-27 21:54:42 +0800

    fix: 移除搜索框轮廓样式并替换样式文件

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // app/layout.tsx
 import type { Metadata } from "next";
 import "./globals.css";
-import "./wetab.css";
+import "./mytab.css";
 
 export const metadata: Metadata = {
   title: "Home",
